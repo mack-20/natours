f5c75c87801dbb2bef2347f53ff4567c64c3a8fa
mack-20 <mackleodebenboulo@gmail.com>
2025-05-20 12:56:01 +0000

    Handle Operational Errors generated by MongoDB: CastError, ValidationError, DuplicateKeyError

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,5 @@
+const AppError = require("../utils/appError")
+
 module.exports = (error, req, res, next) => { 
   // This is done to ensure that if err.status is not set, we have a default state for it. same applies to err.statusCode
   // This is helps standardize error responses
@@ -32,13 +34,41 @@ module.exports = (error, req, res, next) => {
     }
   }
 
+  const handleCastErrorDB = (error) => {
+    const message = `Invalid ${error.path}: ${error.value}`
+    return new AppError(message, 400)
+  }
+
+  const handleValidationErrorDB = (error) => {
+    return new AppError(error.message, 400)
+  }
+
+  const handleDuplicateKeyErrorDB = (error) => {
+    const message = `Tour '${error.keyValue.name}' already exists!`
+    return new AppError(message, 400)
+  }
+
   if(process.env.NODE_ENV === 'development'){
     sendErrorDev(error, res)
   }
   else if (process.env.NODE_ENV === 'production'){
-    sendErrorProd(error, res)
+    let finalError
+
+    // Handle Cast Error
+    if(error.name === 'CastError') finalError = handleCastErrorDB(error)
+
+    // Handle Validation Error
+    if(error.name === 'ValidationError') finalError = handleValidationErrorDB(error)
+
+    // Handle duplicate key error
+    if(error.code === 11000) finalError = handleDuplicateKeyErrorDB(error)
+
+    sendErrorProd(finalError, res)
   }
-
-
   next()
 }
+
+// Errors we need to mark as operational
+// 1. duplicate key : Occurs on unique index violation (e.g. duplicate email).
+// 2. Validation errors: Occurs when data doesn't match the schema.
+// 3. CastError: Occurs when invalid ObjectIds or wrong data types are passed in.
