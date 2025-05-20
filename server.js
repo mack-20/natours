cd92e0e0ee666f0988dc26d2f079aa643ebb9315
mack-20 <mackleodebenboulo@gmail.com>
2025-05-20 13:35:22 +0000

    Handle unhandled Rejections(Promises)

--- server.js
+++ server.js
@@ -8,9 +8,21 @@ dotenv.config({ path: './config.env' })
 
 // Connect mongodb driver to database
 mongoose.connect(process.env.MONGODB_URI.replace('<db_password>', process.env.MONGODB_PASSWORD))
+  .then(() => {console.log('Connection Successful')})
+  .catch((err) => {console.error(`ERROR: ${err}`)})
+
 mongoose.connection.on('connected', () => {console.log('Connected to MongoDB Server')})
 
 // Set up server to listen on port 8000
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`App listening on port ${process.env.PORT}...`)
+})
+
+// handling unhandled promise rejections || safety net
+process.on('unhandledRejection', (err) => {
+  console.error(`ERROR: ${err}`)
+  console.log('Unhandled Exception. Exiting...')
+  server.close(() => {
+    process.exit(1)
+  })
 })
