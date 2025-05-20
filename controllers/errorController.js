e96f79fba47b05a62108d66527e35a472f7c775e
mack-20 <mackleodebenboulo@gmail.com>
2025-05-20 12:13:37 +0000

    Error Handling, development VS production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -4,9 +4,41 @@ module.exports = (error, req, res, next) => {
   error.status = error.status || 'error'
   error.statusCode = error.statusCode || 500
 
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message
-  })
+  const sendErrorDev = (error, res) => {
+    res.status(error.statusCode).json({
+      status: error.status,
+      stack: error.stack,
+      error: error,
+      message: error.message
+    })
+  }
+
+  const sendErrorProd = (error, res) => {
+    // if error is an operational error then we can display to client
+    if (error.isOperational){
+      res.status(error.statusCode).json({
+        status: error.status,
+        message: error.message
+    })  
+    }
+    // else we display generic message to client
+    else{
+      console.error(error)
+
+      res.status(500).json({
+        status: 'error',
+        message: 'Something went wrong!'
+      })
+    }
+  }
+
+  if(process.env.NODE_ENV === 'development'){
+    sendErrorDev(error, res)
+  }
+  else if (process.env.NODE_ENV === 'production'){
+    sendErrorProd(error, res)
+  }
+
+
   next()
 }
