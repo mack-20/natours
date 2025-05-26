664c946513382ad1e5a69853641f3ad9622c8809
mack-20 <mackleodebenboulo@gmail.com>
2025-05-26 14:43:58 +0000

    Create signup route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,2 +1,16 @@
 const User = require('../models/userModel')
+const catchAsync = require('../utils/catchAsync')
 
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body)
+
+  if(newUser){
+    res.status(201).json({
+      status: 'success',
+      message: 'User created successfully',
+      data: {
+        user: newUser
+      }
+    })
+  }
+})
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,12 @@
 const { Router } = require('express')
 const { createUser } = require('../controllers/userControllers')
+const { signup } = require('../controllers/authController')
 
 const userRouter = new Router()
 
 userRouter.route('/').post(createUser)
+
+userRouter.route('/signup').post(signup)
 
 
 
