0a83d2baf6ad9acfa17da18f8b64fad752318a36
mack-20 <mackleodebenboulo@gmail.com>
2025-05-26 15:11:31 +0000

    Hash passwords before storing in DB

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcrypt')
+
 
 // User Schema
 const userSchema = mongoose.Schema({
@@ -30,8 +32,27 @@ const userSchema = mongoose.Schema({
   },
   passwordConfirm:{
     type: String,
-    required: [true, 'Please confirm your password']
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function(passwordConfirm){
+        return passwordConfirm === this.password
+      },
+      message: 'Passwords are not the same'
+    },
   }
+})
+
+// For encryption of data
+userSchema.pre('save', async function(next){
+  if(!this.isModified('password')) return next()
+
+  // hashes the password if the password field was modified
+  this.password = await bcrypt.hash(this.password, 12)
+
+  // gets rid of the passwordConfirm field
+  this.passwordConfirm = undefined
+
+  next()
 })
 
 // Create user model from Schema
